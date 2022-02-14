254e94dc88fb1f72880e7a0c1bcf5fa927fe8a4b
leogdion <leogdion@brightdigit.com>
2022-02-14 16:01:21 -0500

    Upgrading Prch 0.2.0

--- Package.swift
+++ Package.swift
@@ -18,9 +18,9 @@ let package = Package(
     .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.0"), // dev
     .package(url: "https://github.com/realm/SwiftLint", from: "0.43.0"), // dev
     .package(url: "https://github.com/shibapm/Rocket.git", from: "1.2.0"), // dev
-    .package(url: "https://github.com/mattpolzin/swift-test-codecov", .branch("master")), // dev
-    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0"),
-    .package(url: "https://github.com/brightdigit/Prch.git", from: "0.1.0")
+    .package(url: "https://github.com/brightdigit/swift-test-codecov", from: "1.0.0"), // dev
+    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.9.0"),
+    .package(url: "https://github.com/brightdigit/Prch.git", from: "0.2.0-beta.1")
   ],
   targets: [
     .target(name: "PrchNIO", dependencies: ["Prch", .product(name: "AsyncHTTPClient", package: "async-http-client")]),
